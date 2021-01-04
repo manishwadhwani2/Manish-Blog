eab64c0b74e2f44ade1b5eda135f40184b935deb
manishwadhwani2 <46656467+manishwadhwani2@users.noreply.github.com>
2021-01-04 11:54:22 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +129,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
